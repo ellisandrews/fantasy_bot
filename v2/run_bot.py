9c96c2f5d83bbe2b2267dfbba63fcec27bf7f188
Ellis Andrews <ellis@nufitmedia.com>
2018-08-30 01:02:52 -0400

    Stayed up too late, made some incremental progress

--- v2/run_bot.py
+++ v2/run_bot.py
@@ -5,16 +5,13 @@ from pprint import pprint
 
 from slackclient import SlackClient
 
+import slack as sl
 from espn import get_league
-from slack import AT_BOT, add_reaction, BOT_REACTIONS, is_valid_command, is_valid_user, parse_rtm_output
-
-
-SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
 
 
 def listen():
     rtm_output = sc.rtm_read()  # Listen to the Slack output (empty list if no output)
-    bot_output = parse_rtm_output(rtm_output)  # Only grab RTM output that is directed at the bot
+    bot_output = sl.parse_rtm_output(rtm_output)  # Only grab RTM output that is directed at the bot
 
     if bot_output:
 
@@ -25,24 +22,43 @@ def listen():
         # 4) Check if the user passed a week number to inquire about
         # 5) Respond to the user appropriately based on what they requested
 
+        # Get the first name of the user who wrote to the bot
+        user_first_name = sl.get_user_first_name(sc, bot_output)
+
         # Only accept input from known users
-        if not is_valid_user(sc, bot_output):
+        if not sl.validate_user(sc, bot_output, user_first_name):
             return
 
         # Grab the message string directed at '@fantasy_bot', removing the @ mention
-        message = bot_output['text'].replace(AT_BOT, '').strip().lower()
+        message = bot_output['text'].replace(sl.AT_BOT, '').strip().lower()
 
         # Make sure the user messaged the bot with a valid command
-        if not is_valid_command(sc, bot_output, message):
+        if not sl.validate_command(sc, bot_output, user_first_name, message):
             return
 
         # Add an emoji to the Slack message directed at @fantasy_bot
-        add_reaction(sc, bot_output, random.choice(BOT_REACTIONS))
+        sl.add_reaction(sc, bot_output, random.choice(sl.BOT_REACTIONS))
+
+        # See if the user asked about a week (-2 if not, -1 if nonsensical week, or the week number)
+        week = sl.get_week(message)
+        if week == -1:
+            sl.nonsense_week_reponse(sc, bot_output, user_first_name)
+            return
+
+        # At this point, week is either None or the integer week number that it is.
+
+        # Identify which command the user sent to the bot (will pick the first one)
+        command = sl.get_command(message)
+
+        if command == 'matchup':
+            whose_matchup = sl.check_specific_user(message, 'matchup')
+
+            # At this point, whose matchup is either a VALID_USER first name or None (in which case ues user_first_name)
 
 
 if __name__ == "__main__":
 
-    sc = SlackClient(SLACK_API_TOKEN)
+    sc = SlackClient(sl.SLACK_API_TOKEN)
 
     read_websocket_delay = 1  # 1 second delay between reading from the Slack RTM firehose
 
--- v2/slack.py
+++ v2/slack.py
@@ -5,6 +5,8 @@ import os
 from pprint import pprint
 
 
+SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
+
 BOT_ID = os.environ.get('BOT_ID')
 AT_BOT = "<@{bot_id}>".format(bot_id=BOT_ID)
 
@@ -33,23 +35,18 @@ AT_BOT = "<@{bot_id}>".format(bot_id=BOT_ID)
 USERS = [
     'Andrew',
     'Brittany',
-    # 'Charlotte',  # Not playing this year
     'Ellis',
     'Drew',
-    # 'Eunji',  # Not playing this year
     'Grant',
     'Keshav',
     'Kyle',
     'Liz',
     'Mike',
     'Nik',
-    # 'katherine',  # Not playing this year
-    # 'hunter',  # Not playing this year
     'Peter',
     'Regina',
     'Sal',
     'Tiffany'
-    # 'Vishal'  # Not playing this year
 ]
 
 VALID_COMMANDS = ['matchup', 'loser', 'winner', 'scoreboard']
@@ -79,7 +76,7 @@ def parse_rtm_output(rtm_output):
                 return output
 
 
-def get_user_name(slack_client, bot_output):
+def get_user_first_name(slack_client, bot_output):
     """Get the name of the Slack user who messaged the bot."""
     user_id = bot_output['user']
     response = slack_client.api_call('users.info', user=user_id)
@@ -90,25 +87,88 @@ def get_user_name(slack_client, bot_output):
         raise Exception('Slack API call for user name failed')
 
 
-def is_valid_user(slack_client, bot_output):
-    """Check if the user who messaged the bot is a valid user. If they are not, respond to let them know."""
-    user_name = get_user_name(slack_client, bot_output)
-
-    if user_name not in USERS:
-        response = ("Hey {name}! You're not a user I recognize. " 
-                    "Hit up *you're boi* to get credentialed :money_mouth_face:".format(name=user_name))
+def validate_user(slack_client, bot_output, first_name):
+    """Check if the user who messaged the bot is a valid user. Return True if so, False otherwise."""
+    # If they are not a valid user, repsond to them and return False
+    if first_name not in USERS:
+        response = ("Hey {name}! You're not a user I recognize. "
+                    "Hit up *you're boi* to get credentialed :money_mouth_face:".format(name=first_name))
         respond_to_user(slack_client, bot_output, response)
         return False
     else:
         return True
 
 
-def is_valid_command(slack_client, bot_output, message):
+def validate_command(slack_client, bot_output, first_name, message):
     """Check if the user supplied a valid command to the bot. If they did not, respond to them to let them know."""
     if not any(command in message for command in VALID_COMMANDS):
-        user_name = get_user_name(slack_client, bot_output)
-        response = "Not sure what you mean, {name}. I'm just a bot!".format(name=user_name)
+        response = "Not sure what you mean, {name}. I'm just a bot!".format(name=first_name)
         respond_to_user(slack_client, bot_output, response)
         return False
     else:
         return True
+
+
+def get_week(message):
+    """
+    Checks to see if the slack user specified a week of the season in their command (in integer or word form).
+
+    Return (one of):
+        * None if no week found
+        * -1 if the week was non-sensical (<= 0 or > 14)
+        * The week number requested.
+    """
+    if 'week' in message and 'this week' not in message:
+
+        # Get the word/number directly following the word 'week' as a string, stripped of bad characters
+        possible_int_as_str = message.split('week')[1].split()[0].strip().strip('?').strip('!').lower()
+
+        # First check if there is a week number integer in the string
+        week_number = None
+        try:
+            week_number = int(possible_int_as_str)
+        except ValueError:
+            print 'Tried to find a week integer and failed.'
+
+            # Next check if there is a week number word in the string
+            number_words = {
+                'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7,
+                'eight':8,'nine':9,'ten':10, 'eleven':11,'twelve':12,'thirteen':13,'fourteen':14
+            }
+
+            try:
+                week_number = number_words[possible_int_as_str]
+            except KeyError:
+                print 'Tried to find a week number word and failed.'
+                pass
+
+        if week_number is None:
+            return
+        elif week_number <= 0 or week_number > 14:  # If it's a nonsensical week number, return -1
+            return -1
+        else:
+            return week_number
+
+
+def nonsense_week_reponse(slack_client, bot_output, first_name):
+    response = ("Hey, {name} - seems like you're trying to reference a specific week. "
+                "I can only handle regular season, weeks 1 - 13!".format(name=first_name))
+    respond_to_user(slack_client, bot_output, response)
+
+
+def get_command(message):
+    # Find the first valid command keyword issued to the bot
+    words = message.split()
+    for word in words:
+        if word.lower() in VALID_COMMANDS:
+            return word.lower()
+
+
+def check_specific_user(message, keyword):
+    # Get the name of the user right before the keyword (e.g. 'matchup')
+    words = message.split()
+    keyword_index = words.index(keyword) # find the keyword index in the list of words
+    potential_user = words[keyword_index - 1].split("'")[0].title()
+    if potential_user in USERS:
+        return potential_user
+
